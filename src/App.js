d3855a7e2639ebfabaa8a24493a3ee591bb5d68d
Nate Couch <ndcouch@gmail.com>
2018-12-15 01:58:22 -0500

    Completed

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
-import React, { Component } from 'react';
-import './App.css';
-import ToDo from './components/ToDo.js';
+import React, { Component } from "react";
+import "./App.css";
+import ToDo from "./components/ToDo.js";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       todos: [
-        { description: 'Walk the cat', isCompleted: true },
-        { description: 'Throw the dishes away', isCompleted: false },
-        { description: 'Buy new dishes', isCompleted: false }
+        { description: "Walk the cat", isCompleted: true },
+        { description: "Throw the dishes away", isCompleted: false },
+        { description: "Buy new dishes", isCompleted: false }
       ],
-      newTodoDescription: ''
+      newTodoDescription: ""
     };
   }
 
   handleChange(e) {
-    this.setState({ newTodoDescription: e.target.value })
+    this.setState({ newTodoDescription: e.target.value });
   }
 
   handleSubmit(e) {
     e.preventDefault();
-    if (!this.state.newTodoDescription) { return }
-    const newToDo = { description: this.state.newTodoDescription, isCompleted: false };
-    this.setState({ todos: [...this.state.todos, newToDo], newTodoDescription: '' });
+    if (!this.state.newTodoDescription) {
+      return;
+    }
+    const newToDo = {
+      description: this.state.newTodoDescription,
+      isCompleted: false
+    };
+    this.setState({
+      todos: [...this.state.todos, newToDo],
+      newTodoDescription: ""
+    });
   }
 
   toggleComplete(index) {
@@ -34,25 +41,45 @@ class App extends Component {
     this.setState({ todos: todos });
   }
 
-  handleDelete(index) {
+  /* handleDelete(index) {
     const todos = this.state.todos.slice();
     const todo = todos[index];
     todo.isCompleted = todo.isCompleted ? false : true;
     this.setState({ todos: todos });
-  }
+  }*/
 
+  handleDelete = index => {
+    const { todos } = this.state;
+
+    this.setState({
+      todos: todos.filter((item, i) => {
+        console.log(item);
+        return i !== index;
+      })
+    });
+  };
 
   render() {
     return (
       <div className="App">
         <ul>
-          {this.state.todos.map((todo, index) =>
-            <ToDo key={index} description={todo.description} isCompleted={todo.isCompleted} toggleComplete={() => this.toggleComplete(index)} handleDelete={() => this.handleDelete(index)} />
-          )}
+          {this.state.todos.map((todo, index) => (
+            <ToDo
+              key={index}
+              description={todo.description}
+              isCompleted={todo.isCompleted}
+              toggleComplete={() => this.toggleComplete(index)}
+              handleDelete={() => this.handleDelete(index)}
+            />
+          ))}
         </ul>
 
-        <form onSubmit={(e) => this.handleSubmit(e)}>
-          <input type="text" value={this.state.newTodoDescription} onChange={(e) => this.handleChange(e)} />
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <input
+            type="text"
+            value={this.state.newTodoDescription}
+            onChange={e => this.handleChange(e)}
+          />
           <input type="submit" />
         </form>
       </div>
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,13 +1,17 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class ToDo extends Component {
   render() {
     return (
-        <li>
-            <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
-            <span>{ this.props.description }</span>
-            <button onClick={this.props.handleDelete}>Remove</button>
-        </li>
+      <li>
+        <input
+          type="checkbox"
+          checked={this.props.isCompleted}
+          onChange={this.props.toggleComplete}
+        />
+        <span>{this.props.description}</span>
+        <button onClick={this.props.handleDelete}>Remove</button>
+      </li>
     );
   }
 }
